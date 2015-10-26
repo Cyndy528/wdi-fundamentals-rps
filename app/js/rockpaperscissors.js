c97d50dbf01809dd659fd95c7787071292720f77
Cynthia Salinas <csalinas123@gmail.com>
2015-10-26 11:05:37 -0700

    wdifundamentals

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,20 +1,20 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
-
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+   console.log("Please choose either 'rock', 'paper', or 'scissors'.");
+   var answer = prompt();
+   return answer;
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
-        return "rock";
+        return 'rock';
     } else if (randomNumber < 0.66) {
-        return "paper";
+        return 'paper';
     } else {
-        return "scissors";
+        return 'scissors';
     }
 }
 ////////////////////////////////////////////////
@@ -25,104 +25,108 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-	return move = move || getInput(); 
+
+        if (move !== null) {
+            move = move;  
+        }  
+        else {
+            move = getInput();
+        }               
+                        
+        return move;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    	return move = move || randomPlay(); 
-}
+        if (move !== null){
+            move = move;    
+        }
+        else { 
+            move = randomPlay();
+        }
+
+    console.log("Computer Chose" + move);
+    return move;
+
+} 
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+        
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    	
-        var userChoice;
-        userChoice = prompt ("Do you choose rock, paper or scissors?");
-        var computerChoice;
+    if (playerMove == "rock" && computerMove == "paper"){ 
+        winner = "computer";
+        console.log("Computer Wins!"); 
+    }                      
 
-        computerChoice = Math.random();
-        console.log(computerChoice);
-        document.write(computerChoice);
-
-        if (computerChoice < 0.34) {
-                computerChoice = "rock";
-}       else if (computerChoice <= 0.67) {
-                computerChoice = "paper";
-}       else {computerChoice = "scissors";
-}
-
-choice1 = userChoice;
-choice2 = computerChoice;
-
-function compare(choice1, choice2) {
-    if(choice1 === choice2) {
-    return ("The result is a tie!");
+	else if(playerMove == "paper" && computerMove == "rock"){
+        winner = "player";
+        console.log("Player Wins!"); 
     }
-
-    if(choice1 === "rock") {
-
-        if(choice2 === "scissors") {
-        return ("rock wins!");
+    else if(playerMove == "paper" && computerMove == "scissors"){
+        winner = "computer";
+        console.log("Computer Wins!");  
     }
-    else {
-        return ("paper wins!");
-        }
-
-    if (choice1 === "paper") {
-        if (choice2 === "rock") {
-        return ("paper wins!");
+    else if(playerMove == "scissors" && computerMove == "paper"){
+        winner = "player";
+        console.log("Player Wins!"); 
     }
-
-    else {
-        return ("scissors wins!");
-        }
-    } 
-    if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-        return ("rock wins!");
+    else if(playerMove == "scissors" && computerMove == "rock"){
+        winner = "computer";
+        console.log("Computer Wins!"); 
     }
+    else if(playerMove == "rock" && computerMove == "scissors"){
+        winner = "player";
+        console.log("Player Wins!"); 
+    }
+    else{
+        winner = "tie";
+        console.log("It's a tie!");
+    }
+    return winner; 
+    }
+ 
 
-    else {
-        return ("scissors wins!");
-        }
-    } 
-}
-
-console.log(compare(choice1,choice2));
-
-	
-function playToFive() {
+function playToFive(){
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    
-  playNewGame();
-    	if (computerWins === 5) {
-        alert("Computer Wins");
-        if (confirm("Play Again?")) {
-            resetGame();
-        } else{
-            alert("End Game");
+   // This function should continue to play Rock Paper Scissors until either the
+  // player or the computer has won five times.
+  // After each 'round', display some text in the console indicating who played
+  // what, who won, and what the current scoreboard looks like.
+  // For example,
+  //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+  //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+  /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5){
+        var winner = getWinner(getPlayerMove(), getComputerMove());
+
+        if (winner == "player"){
+            playerWins++;
+            console.log([playerWins, computerWins]); 
         }
-    } else if (playerWins === 5) {
-        alert("Player Wins");
-        if (confirm("Play Again?")) {
-            resetGame();
-        } else{
-            alert("End Game");
-        }  
-    } else {
-        playToFive();
-        
-        return [playerWins, computerWins];
-	}
-}
+        else if (winner == "computer"){
+            computerWins++; 
+            console.log([playerWins, computerWins]); 
+        }
+        else{
+            console.log("Score is the same"); 
+        }
+    }
+        if(playerWins == 5){
+            console.log("Player Wins Game!");    
+        }
+        else {
+            console.log("Computer Wins Game!"); 
+        }
+        return [playerWins, computerWins]; 
+  }
+
+ 
 
 
 
---
+++ test/spec/scipts.js
@@ -0,0 +1,78 @@
+// Initialize variables
+var keepPlaying = true;
+var userChoice = null;
+var compChoice = null;
+var userWins = 0;
+var compWins = 0;
+
+// Returns rock, paper, or scissors randomly
+function getCompChoice () {
+	var randomInt = Math.floor(Math.random() * 3);
+	if (randomInt == 0) {
+		return "rock";
+	} else if (randomInt == 1) {
+		return "paper";
+	} else {
+		return "scissors";
+	}
+}
+
+// If user wins
+function youWin () {
+	userWins++;
+	alert("You win! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+	return true;
+}
+
+// If user loses
+function youLose () {
+	compWins++;
+	alert("You lose! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+	return true;
+}
+
+// Tie
+function tie () {
+	alert("It's a tie! The computer also chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+	return true;
+}
+
+// Main while loop
+while ( keepPlaying == true ) {
+
+	// Set compChoice
+	var compChoice = getCompChoice();
+
+	// Set userChoice
+	var userChoice = prompt("Choose rock, paper, or scissors:");
+
+	if ( userChoice == "rock" || userChoice == "paper" || userChoice == "scissors" ) {
+		if (userChoice == compChoice) {
+			tie();
+		}
+		else if (userChoice == "rock") {
+			if (compChoice == "paper") {
+				youLose();
+			} else {
+				youWin();
+			}
+		} else if (userChoice == "paper") {
+			if (compChoice == "rock") {
+				youWin();
+			} else {
+				youLose();
+			}
+		} else {
+			if (compChoice == "rock") {
+				youLose();
+			} else {
+				youWin();
+			}
+		}
+	} else {
+		alert("Choose either rock, paper, or scissors.");
+		return false;
+	}
+
+	keepPlaying = confirm("Do you want to keep playing?");
+}
